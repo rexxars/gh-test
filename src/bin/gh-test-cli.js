cf6abe954e4f152df7d695db6bcc051371544eda
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-09-20 12:02:31 +0200

    Testing

---
+++ src/bin/gh-test-cli.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
+const argv = process.argv.map(thing => thing.split('=')).flat()
+const dirAt = argv.indexOf('--dir')
+const dir = dirAt === -1 ? process.cwd() : path.resolve(process.cwd(), argv[dirAt + 1])
+
+console.log('We are running!')
+console.log('Directory: %s\n', dir)
+
+console.log('Files/directories in here:')
+console.log(fs.readdirSync(dir).map(file => ` - ${file}`).join('\n'))
+console.log('')
